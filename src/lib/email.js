e2d615bbc7195d9e0f314c42955fc8e8def88305
MartinAnDoMan03 <martinadmanalu@gmail.com>
2026-08-01 09:44:45 +0700

    email configuration logging

--- src/lib/email.js
+++ src/lib/email.js
@@ -17,9 +17,9 @@ const transporter = nodemailer.createTransport(emailConfig);
 // Verify connection configuration
 transporter.verify((error, success) => {
   if (error) {
-    // console.log('Email configuration error:', error);
+    console.log('Email configuration error:', error);
   } else {
-    // console.log('Email server is ready to take our messages');
+    console.log('Email server is ready to take our messages');
   }
 });
 
