eeae8fc053750041808910b8e5befd46cb1a6ece
MartinAnDoMan03 <martinadmanalu@gmail.com>
2026-08-27 06:49:06 +0700

    growth stat removal

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -377,8 +377,6 @@ export default function DashboardOverview() {
               <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/>
             </svg>
           }
-          change={stats.sellerGrowth}
-          changeType={stats.sellerGrowth >= 0 ? 'positive' : 'negative'}
         />
         <StatCard
           title="Active Sellers"
@@ -406,8 +404,6 @@ export default function DashboardOverview() {
               <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd"/>
             </svg>
           }
-          change={stats.buyerGrowth}
-          changeType={stats.buyerGrowth >= 0 ? 'positive' : 'negative'}
         />
       </div>
 
@@ -420,8 +416,6 @@ export default function DashboardOverview() {
               <path fillRule="evenodd" d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z" clipRule="evenodd"/>
             </svg>
           }
-          change={stats.monthlyGrowth}
-          changeType={stats.monthlyGrowth >= 0 ? 'positive' : 'negative'}
         />
         <StatCard
           title="Total Revenue"
@@ -432,8 +426,6 @@ export default function DashboardOverview() {
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd"/>
             </svg>
           }
-          change={stats.revenueGrowth}
-          changeType={stats.revenueGrowth >= 0 ? 'positive' : 'negative'}
         />
         <StatCard
           title="Monthly Growth"
